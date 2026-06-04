7d8cc121e3743ac7876ea1aa33dad9b0a2316881
Alexis <alexisb1v1@gmail.com>
2026-06-04 11:21:29 -0500

    refactor: escuchar en 0.0.0.0 de forma explicita en main.ts para redes docker

--- src/main.ts
+++ src/main.ts
@@ -75,7 +75,7 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   // Imprimir logs de conexión ultra limpios y claros solicitados por el usuario
   console.log('\n================ GpsApiCentral ===============');
