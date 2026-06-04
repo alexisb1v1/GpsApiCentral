d337fbc8d982d55a798dfb327deea9c9284e53a7
Alexis <alexisb1v1@gmail.com>
2026-06-03 20:17:47 -0500

    fix(webhook): add validation decorators to TraccarWebhookRequestDto to resolve ValidationPipe rejection

--- src/tracking/interfaces/http/v1/traccar-webhook/dto/traccar-webhook.request.dto.ts
+++ src/tracking/interfaces/http/v1/traccar-webhook/dto/traccar-webhook.request.dto.ts
@@ -1,39 +1,67 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsNumber, ValidateNested, IsObject } from 'class-validator';
+import { Type } from 'class-transformer';
 
 class TraccarEventDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   type: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
   deviceId: number;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
   geofenceId: number;
 }
 
 class TraccarPositionDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   fixTime: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
   latitude: number;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
   longitude: number;
 }
 
 class TraccarDeviceDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   uniqueId: string;
 }
 
 export class TraccarWebhookRequestDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => TraccarEventDto)
   event: TraccarEventDto;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => TraccarPositionDto)
   position: TraccarPositionDto;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => TraccarDeviceDto)
   device: TraccarDeviceDto;
 }
